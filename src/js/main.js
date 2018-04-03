1f43f91b3b58a5a0f957ca8b23d36699d0bc93ea
Jon Arild Karlsen <karlsen.jonarild@gmail.com>
2018-04-03 22:47:03 +0200

    FEATURE: cat selection

--- src/js/main.js
+++ src/js/main.js
@@ -2,38 +2,60 @@
 
 /* Variables */
 const cats = [ 
-		{
-			name: 'simba',
-			counter: 0
-		},
-		{
-			name: 'nala',
-			counter: 0
-		} 
-	],
-	catHTML = [];
+	{
+		name: 'simba',
+	},
+	{
+		name: 'nala',
+	},
+	{
+		name: 'luna',
+	},
+	{
+		name: 'daisy',
+	},
+	{
+		name: 'bell',
+	}
+];
 
 /* Run on DOM ready 
 */
 $(function () {
-	/* Create cat, name, click count HTML
-	*/
+	/* Create list of cat names
+	 */
 	(function() {
 		for (let cat of cats) {
-			catHTML.push(`<figure class="cat-img">
+			$('.cat-selection ul').append(`<li>${cat.name}</li>`);
+		}
+	})();
+	
+	/* Create cat HTML
+	 */
+	(function() {
+		for (let cat of cats) {
+			cat.html = `<figure class="cat-img hide">
 			<p class="cat-name">${cat.name}</p>
 			<img src="/images/${cat.name}.jpg" alt="a cat" id="${cat.name}">
 			<figcaption><p>0</p></figcaption>
-			</figure>`);
+			</figure>`;
 		}
 
-		for (let element of catHTML) {
-			$('.cats').append(element);
+		for (let cat of cats) {
+			$('.cats').append(cat.html);
 		}
 	})();
 
+	/* Increment cat's associated counter on click
+	 */
 	$('.cat-img').on('click', (e) => {
 		let currentCounter = parseInt($(e.currentTarget).find('figcaption').text());
 		$(e.currentTarget).find('figcaption').text(++currentCounter); 
+	});
+
+	$('.cat-selection li').on('click', (e) => {
+		let id = `#${$(e.currentTarget).text()}`;
+		console.log($(id));
+		$(id).parent().toggleClass('hide');
 	});
 });
